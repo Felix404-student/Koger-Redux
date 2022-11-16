028a8c6e981d61dfd78f672d9f4ce66742aeef5f
Felix404-student <justhbrown@gmail.com>
2022-11-16 14:22:26 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Koger-Redux',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
